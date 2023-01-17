04e612641f81fface712d64fa9818bc32332e426
Charles Dixon <chvckd@gmail.com>
2023-01-17 12:10:29 +0000

    Add naive conn manager implementation

--- connmgr.go
+++ connmgr.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"crypto/tls"
+	"github.com/couchbase/stellar-nebula/core/memdx"
+	"math/rand"
 	"sync"
 
 	"golang.org/x/exp/slices"
@@ -14,19 +17,56 @@ type ConnectionManager interface {
 }
 
 type connMgrPipeline struct {
-	connections []*KvClient
+	connections []KvClient
+}
+
+func (pipeline *connMgrPipeline) Close() error {
+	for _, conn := range pipeline.connections {
+		conn.Close()
+	}
+
+	return nil
 }
 
 type connectionManager struct {
 	lock        sync.Mutex
 	connections map[string]*connMgrPipeline
+
+	connectionsPerNode uint8
+
+	tlsConfig *tls.Config
+	bucket    string
+
+	username string
+	password string
+
+	requestedFeatures []memdx.HelloFeature
 }
 
 var _ ConnectionManager = (*connectionManager)(nil)
 
-func newConnectionManager() *connectionManager {
+type connManagerOptions struct {
+	ConnectionsPerNode uint8
+
+	TlsConfig      *tls.Config
+	SelectedBucket string
+	Features       []memdx.HelloFeature
+
+	// TODO: this should probably be swapped out for an authenticator of some sort, which can probably be hotswapped
+	// during Reconfigure.
+	Username string
+	Password string
+}
+
+func newConnectionManager(opts connManagerOptions) *connectionManager {
 	return &connectionManager{
-		connections: make(map[string]*connMgrPipeline),
+		connections:        make(map[string]*connMgrPipeline),
+		connectionsPerNode: opts.ConnectionsPerNode,
+		tlsConfig:          opts.TlsConfig,
+		requestedFeatures:  opts.Features,
+		username:           opts.Username,
+		password:           opts.Password,
+		bucket:             opts.SelectedBucket,
 	}
 }
 
@@ -35,13 +75,36 @@ func (m *connectionManager) UpdateEndpoints(endpoints []string) error {
 
 	for _, endpoint := range endpoints {
 		if m.connections[endpoint] == nil {
-			// this pipeline needs to be created...
+			clients := make([]KvClient, m.connectionsPerNode)
+			for i := uint8(0); i < m.connectionsPerNode; i++ {
+				cli, err := newKvClient(kvClientOptions{
+					Hostname:       endpoint,
+					TlsConfig:      m.tlsConfig,
+					SelectedBucket: m.bucket,
+					Features:       m.requestedFeatures,
+					Username:       m.username,
+					Password:       m.password,
+				})
+				if err != nil {
+					m.lock.Unlock()
+					return err // TODO: not really sure what we'd do here...
+				}
+
+				_, err = cli.Bootstrap()
+				if err != nil {
+					m.lock.Unlock()
+					return err // TODO: not really sure what we'd do here...
+				}
+
+				clients[i] = cli
+			}
+			m.connections[endpoint] = &connMgrPipeline{connections: clients}
 		}
 	}
 
-	for endpoint := range m.connections {
+	for endpoint, pipeline := range m.connections {
 		if !slices.Contains(endpoints, endpoint) {
-			// this pipeline should be destroyed
+			pipeline.Close()
 		}
 	}
 
@@ -51,5 +114,13 @@ func (m *connectionManager) UpdateEndpoints(endpoints []string) error {
 }
 
 func (m *connectionManager) Execute(endpoint string, handler ConnExecuteHandler) error {
-	return nil
+	m.lock.Lock()
+	pipeline, ok := m.connections[endpoint]
+	if !ok {
+		return placeholderError{Inner: "endpoint not known"}
+	}
+	client := pipeline.connections[rand.Intn(len(pipeline.connections))]
+	m.lock.Unlock()
+
+	return handler(client, nil)
 }
