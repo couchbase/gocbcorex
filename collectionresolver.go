e55eabb6e645a456f5a516c7cf319c85d30539d7
Brett Lawson <brett19@gmail.com>
2023-01-24 13:42:07 -0800

    Modified collection resolver to only retry when deterministic.

--- collectionresolver.go
+++ collectionresolver.go
@@ -18,23 +18,50 @@ func OrchestrateMemdCollectionID[RespT any](
 	scopeName, collectionName string,
 	fn func(collectionID uint32, manifestID uint64) (RespT, error),
 ) (RespT, error) {
-	collectionID, manifestID, err := cr.ResolveCollectionID(ctx, scopeName, collectionName)
+	collectionID, manifestRev, err := cr.ResolveCollectionID(ctx, scopeName, collectionName)
 	if err != nil {
 		var emptyResp RespT
 		return emptyResp, err
 	}
 
 	for {
-		res, err := fn(collectionID, manifestID)
+		res, err := fn(collectionID, manifestRev)
 		if err != nil {
 			if errors.Is(err, memdx.ErrUnknownCollectionID) {
-				// TODO(brett19): Implement the endpoint and collectionID info here...
-				cr.InvalidateCollectionID(ctx, scopeName, collectionName, "", 0)
+				invalidatingEndpoint := ""
+				invalidatingManifestRev := uint64(0)
 
-				// TODO(brett19): Implement actually retrying when possible.
-				// Note that we need to return the error if our second resolution yields
-				// the same information we already had rather than looping.  In case the
-				// server is "perpetually behind".
+				var serverErr memdx.ServerError
+				if errors.As(err, &serverErr) {
+					serverCtx := serverErr.ParseContext()
+					invalidatingManifestRev = serverCtx.ManifestRev
+				}
+
+				cr.InvalidateCollectionID(
+					ctx,
+					scopeName, collectionName,
+					invalidatingEndpoint, invalidatingManifestRev)
+
+				newCollectionID, newManifestRev, newResolveErr :=
+					cr.ResolveCollectionID(ctx, scopeName, collectionName)
+				if newResolveErr != nil {
+					var emptyResp RespT
+					return emptyResp, newResolveErr
+				}
+
+				if newCollectionID == collectionID {
+					// if resolution yielded the same response, this means that our ability
+					// to fetch an updated collection id is compromised, or the server is in
+					// an older state.  In both instances, we no longer have a deterministic
+					// path to resolution and return the error, allowing retries to occur
+					// at a higher level if desired.
+					var emptyResp RespT
+					return emptyResp, err
+				}
+
+				collectionID = newCollectionID
+				manifestRev = newManifestRev
+				continue
 			}
 
 			return res, err
