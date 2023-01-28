5cb1f6c32d8fe77492249cc911b7f5aacad30cf3
Brett Lawson <brett19@gmail.com>
2023-01-27 16:45:37 -0800

    Added a simple get operation benchmark to tests.

--- agent_test.go
+++ agent_test.go
@@ -45,3 +45,38 @@ func TestBasicSnAgent(t *testing.T) {
 	assert.NotZero(t, getRes.Cas)
 	assert.NotEmpty(t, getRes.Value)
 }
+
+func BenchmarkBasicGet(b *testing.B) {
+	opts := AgentOptions{
+		TLSConfig:  nil,
+		BucketName: "default",
+		Username:   "Administrator",
+		Password:   "password",
+		HTTPAddrs:  testutils.TestOpts.HTTPAddrs,
+		MemdAddrs:  testutils.TestOpts.MemdAddrs,
+	}
+
+	agent, err := CreateAgent(context.Background(), opts)
+	if err != nil {
+		b.Errorf("failed to create agent: %s", err)
+	}
+
+	_, err = agent.Upsert(context.Background(), &UpsertOptions{
+		Key:            []byte("test"),
+		ScopeName:      "",
+		CollectionName: "",
+		Value:          []byte(`{"foo": "bar"}`),
+	})
+	if err != nil {
+		b.Errorf("failed to upsert test document: %s", err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		agent.Get(context.Background(), &GetOptions{
+			Key:            []byte("test"),
+			ScopeName:      "",
+			CollectionName: "",
+		})
+	}
+	b.ReportAllocs()
+}
