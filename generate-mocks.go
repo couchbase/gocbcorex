25b17f42c27ccc2543a419569787124016762d1f
Charles Dixon <chvckd@gmail.com>
2023-01-30 15:37:58 +0000

    Add kvclientmanager tests

--- generate-mocks.go
+++ generate-mocks.go
@@ -2,6 +2,8 @@
 //go:generate moq -out mock_collectionresolver_test.go . CollectionResolver
 //go:generate moq -out mock_vbucketrouter_test.go . VbucketRouter
 //go:generate moq -out mock_kvclient_test.go . KvClient
+//go:generate moq -out mock_kvclientpool_test.go . KvClientPool
+//go:generate moq -out mock_kvclientmanager_test.go . KvClientManager
 //go:generate moq -out mock_retrymanager_test.go . RetryManager RetryController
 
 package core
