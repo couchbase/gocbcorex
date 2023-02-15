7f67ff39170db18eb59ff5ef3fe1b6fd87996d52
Charles Dixon <chvckd@gmail.com>
2023-02-15 11:55:17 +0000

    Add support for durability to memdx

---
+++ memdx/durability.go
@@ -0,0 +1,16 @@
+package memdx
+
+// DurabilityLevel specifies the level to use for enhanced durability requirements.
+type DurabilityLevel uint8
+
+const (
+	// DurabilityLevelMajority indicates the operation must be replicated to the majority.
+	DurabilityLevelMajority = DurabilityLevel(1)
+
+	// DurabilityLevelMajorityAndPersistToActive indicates the operation must be replicated
+	// to the majority and persisted to the active server.
+	DurabilityLevelMajorityAndPersistToActive = DurabilityLevel(2)
+
+	// DurabilityLevelPersistToMajority indicates the operation must be persisted to the active server.
+	DurabilityLevelPersistToMajority = DurabilityLevel(3)
+)
