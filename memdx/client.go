6f74359a97ba828f4013681f52f545e2f63e092c
Brett Lawson <brett19@gmail.com>
2023-05-31 16:38:29 -0700

    Improved debug logging of packets in memdx.

--- memdx/client.go
+++ memdx/client.go
@@ -123,8 +123,17 @@ func (c *Client) cancelHandler(opaqueID uint32, err error) {
 func (c *Client) dispatchCallback(pak *Packet) error {
 	if enablePacketLogging {
 		c.logger.Debug("read packet",
-			zap.Uint32("opaque", pak.Opaque),
+			zap.String("magic", pak.Magic.String()),
 			zap.String("opcode", pak.OpCode.String()),
+			zap.Uint8("datatype", pak.Datatype),
+			zap.Uint16("vbucketID", pak.VbucketID),
+			zap.String("status", pak.Status.String()),
+			zap.Uint32("opaque", pak.Opaque),
+			zap.Uint64("cas", pak.Cas),
+			zap.Binary("extras", pak.Extras),
+			zap.Binary("key", pak.Key),
+			zap.Binary("value", pak.Value),
+			zap.Binary("framingExtras", pak.FramingExtras),
 		)
 	}
 
@@ -193,8 +202,17 @@ func (c *Client) Dispatch(req *Packet, handler DispatchCallback) (PendingOp, err
 
 	if enablePacketLogging {
 		c.logger.Debug("writing packet",
-			zap.Uint32("opaque", req.Opaque),
+			zap.String("magic", req.Magic.String()),
 			zap.String("opcode", req.OpCode.String()),
+			zap.Uint8("datatype", req.Datatype),
+			zap.Uint16("vbucketID", req.VbucketID),
+			zap.String("status", req.Status.String()),
+			zap.Uint32("opaque", req.Opaque),
+			zap.Uint64("cas", req.Cas),
+			zap.Binary("extras", req.Extras),
+			zap.Binary("key", req.Key),
+			zap.Binary("value", req.Value),
+			zap.Binary("framingExtras", req.FramingExtras),
 		)
 	}
 
