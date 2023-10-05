977feeb44ba447b75ec3c58328ce228f379475a6
Charles Dixon <chvckd@gmail.com>
2023-10-05 15:11:12 +0100

    ING-588: Refactor buckets tracking agent manager
    Motivation
    -----------
    At the moment the buckets tracking agent manager will never ever
    return a bucket not found error for a GetAgent call. We need
    to rework the manager so that it returns bucket not found if the
    bucket does not exist on the cluster, rather than waiting until
    context is errored.
    
    Changes
    -------
    Refactor a buckets tracking watcher out of the buckets tracking
    agent manager.
    Update bucket tracking watcher so that when GetAgent is called
    for an unknown bucket then it triggers the watcher to attempt
    a manual update.
    Update watcher logic so that if the request to GetAgent was made
    after a manual update was already in flight then it will trigger
    another manual update if the bucket is still unknown afterward.

--- bucketstracking_agentmanager.go
+++ bucketstracking_agentmanager.go
@@ -7,10 +7,6 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/couchbase/gocbcorex/cbmgmtx"
-
-	"golang.org/x/exp/slices"
-
 	"go.uber.org/zap"
 )
 
@@ -39,20 +35,21 @@ type bucketsTrackingAgentManagerState struct {
 }
 
 type BucketsTrackingAgentManager struct {
-	bucketsLock sync.Mutex
 	stateLock   sync.Mutex
-	opts        BucketsTrackingAgentManagerOptions
 	logger      *zap.Logger
 	userAgent   string
 	networkType string
 
+	compressionConfig  CompressionConfig
+	configPollerConfig ConfigPollerConfig
+	httpConfig         HTTPConfig
+
 	state *bucketsTrackingAgentManagerState
 
 	clusterAgent *Agent
-	bucketAgents map[string]*Agent
 	closed       bool
 
-	bucketsWatcher *StreamWatcherHttp[[]bucketDescriptor]
+	bucketsWatcher *BucketsWatcherHttp
 	watchersCancel func()
 
 	topologyCfgWatcher *ConfigWatcherHttp
@@ -69,7 +66,7 @@ func CreateBucketsTrackingAgentManager(ctx context.Context, opts BucketsTracking
 	httpUserAgent := "gocbcorex/0.0.1-dev"
 
 	logger := loggerOrNop(opts.Logger)
-	logger = logger.Named("agent manager")
+	logger = logger.Named("tracking-agent-manager")
 
 	bootstrapper, err := NewConfigBootstrapHttp(ConfigBoostrapHttpOptions{
 		Logger:           logger.Named("http-bootstrap"),
@@ -92,25 +89,34 @@ func CreateBucketsTrackingAgentManager(ctx context.Context, opts BucketsTracking
 		zap.String("networkType", networkType))
 
 	m := &BucketsTrackingAgentManager{
-		opts:         opts,
-		logger:       logger,
-		userAgent:    httpUserAgent,
-		networkType:  networkType,
-		bucketAgents: make(map[string]*Agent),
+		logger:      logger,
+		userAgent:   httpUserAgent,
+		networkType: networkType,
+
+		compressionConfig:  opts.CompressionConfig,
+		configPollerConfig: opts.ConfigPollerConfig,
+		httpConfig:         opts.HTTPConfig,
+
 		state: &bucketsTrackingAgentManagerState{
 			tlsConfig:     opts.TLSConfig,
 			authenticator: opts.Authenticator,
 			httpTransport: httpTransport,
 			latestConfig:  bootstrapConfig,
 		},
+		clusterAgent:       nil,
+		closed:             false,
+		bucketsWatcher:     nil,
+		watchersCancel:     nil,
+		topologyCfgWatcher: nil,
 	}
 
-	bucketsWatcher, err := NewStreamWatcherHttp[[]bucketDescriptor](&StreamWatcherHttpConfig{
+	bucketsWatcher, err := NewBucketsWatcherHttp(BucketsWatcherHttpConfig{
 		HttpRoundTripper: httpTransport,
 		Endpoints:        srcHTTPAddrs,
 		UserAgent:        httpUserAgent,
 		Authenticator:    opts.Authenticator,
-	}, &StreamWatcherHttpOptions{
+		MakeAgent:        m.makeAgent,
+	}, BucketsWatcherHttpOptions{
 		Logger: logger.Named("buckets-watcher"),
 	})
 	if err != nil {
@@ -135,7 +141,7 @@ func CreateBucketsTrackingAgentManager(ctx context.Context, opts BucketsTracking
 
 	m.topologyCfgWatcher = topoWatcher
 
-	clusterAgent, err := m.makeAgentLocked(ctx, "")
+	clusterAgent, err := m.makeAgent(ctx, "")
 	if err != nil {
 		return nil, err
 	}
@@ -151,22 +157,15 @@ func (m *BucketsTrackingAgentManager) startWatchers() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	stoppedTopoSig := make(chan struct{})
-	stoppedBucketSig := make(chan struct{})
 	m.watchersCancel = func() {
 		// Cancel the context and wait for the configCh to be closed.
 		cancel()
-		<-stoppedBucketSig
+		m.logger.Debug("Waiting for topology watcher shutdown")
 		<-stoppedTopoSig
+		m.logger.Debug("Completed topology watcher shutdown")
 	}
 
-	bucketCh := m.bucketsWatcher.Watch(ctx, streamWatcherHttp_streamBuckets)
-
-	go func() {
-		for buckets := range bucketCh {
-			m.handleBuckets(buckets)
-		}
-		close(stoppedBucketSig)
-	}()
+	m.bucketsWatcher.Watch()
 
 	topoCh := m.topologyCfgWatcher.Watch(ctx)
 
@@ -178,7 +177,7 @@ func (m *BucketsTrackingAgentManager) startWatchers() {
 	}()
 }
 
-func (m *BucketsTrackingAgentManager) mgmtEndpoints() []string {
+func (m *BucketsTrackingAgentManager) mgmtEndpointsLocked() []string {
 	bootstrapHosts := m.state.latestConfig.AddressesGroupForNetworkType(m.networkType)
 
 	var mgmtEndpoints []string
@@ -200,7 +199,7 @@ func (m *BucketsTrackingAgentManager) applyConfig(config *ParsedConfig) {
 	m.stateLock.Lock()
 	defer m.stateLock.Unlock()
 
-	if !canUpdateConfig(config, m.state.latestConfig, m.opts.Logger) {
+	if !canUpdateConfig(config, m.state.latestConfig, m.logger) {
 		return
 	}
 
@@ -208,7 +207,7 @@ func (m *BucketsTrackingAgentManager) applyConfig(config *ParsedConfig) {
 
 	m.state.latestConfig = config
 
-	mgmtEndpoints := m.mgmtEndpoints()
+	mgmtEndpoints := m.mgmtEndpointsLocked()
 
 	m.topologyCfgWatcher.Reconfigure(&ConfigWatcherHttpConfig{
 		HttpRoundTripper: m.state.httpTransport,
@@ -217,50 +216,42 @@ func (m *BucketsTrackingAgentManager) applyConfig(config *ParsedConfig) {
 		Authenticator:    m.state.authenticator,
 	})
 
-	m.bucketsWatcher.Reconfigure(&StreamWatcherHttpConfig{
+	m.bucketsWatcher.Reconfigure(&BucketsWatcherHttpReconfigureConfig{
 		HttpRoundTripper: m.state.httpTransport,
 		Endpoints:        mgmtEndpoints,
-		UserAgent:        m.userAgent,
 		Authenticator:    m.state.authenticator,
 	})
 }
 
-func (m *BucketsTrackingAgentManager) handleBuckets(buckets []bucketDescriptor) {
-	m.bucketsLock.Lock()
-	defer m.bucketsLock.Unlock()
-	for _, bucket := range buckets {
-		if _, ok := m.bucketAgents[bucket.Name]; !ok {
-			m.opts.Logger.Debug("New bucket on cluster, creating agent", zap.String("name", bucket.Name))
-			agent, err := m.makeAgentLocked(context.Background(), bucket.Name)
-			if err != nil {
-				m.opts.Logger.Debug("Failed to create agent", zap.String("name", bucket.Name))
-				continue
-			}
+func (m *BucketsTrackingAgentManager) makeAgent(ctx context.Context, bucketName string) (*Agent, error) {
+	m.stateLock.Lock()
+	defer m.stateLock.Unlock()
 
-			m.bucketAgents[bucket.Name] = agent
-		}
+	bootstrapHosts := m.state.latestConfig.AddressesGroupForNetworkType(m.networkType)
+
+	var kvDataHosts []string
+	var mgmtEndpoints []string
+
+	tlsConfig := m.state.tlsConfig
+	if tlsConfig == nil {
+		kvDataHosts = bootstrapHosts.NonSSL.KvData
+		mgmtEndpoints = bootstrapHosts.NonSSL.Mgmt
+	} else {
+		kvDataHosts = bootstrapHosts.SSL.KvData
+		mgmtEndpoints = bootstrapHosts.SSL.Mgmt
 	}
 
-	for bucket, agent := range m.bucketAgents {
-		if !slices.ContainsFunc(buckets, func(descriptor bucketDescriptor) bool {
-			return descriptor.Name == bucket
-		}) {
-			m.opts.Logger.Debug("Bucket no longer on cluster, shutting down agent", zap.String("name", bucket))
-			delete(m.bucketAgents, bucket)
-			agent.Close()
-		}
-	}
-}
-
-func (m *BucketsTrackingAgentManager) makeAgentLocked(ctx context.Context, bucketName string) (*Agent, error) {
 	return CreateAgent(ctx, AgentOptions{
-		Logger:             m.opts.Logger,
-		TLSConfig:          m.opts.TLSConfig,
-		Authenticator:      m.opts.Authenticator,
-		SeedConfig:         m.opts.SeedConfig,
-		CompressionConfig:  m.opts.CompressionConfig,
-		ConfigPollerConfig: m.opts.ConfigPollerConfig,
-		HTTPConfig:         m.opts.HTTPConfig,
+		Logger:        m.logger,
+		TLSConfig:     m.state.tlsConfig,
+		Authenticator: m.state.authenticator,
+		SeedConfig: SeedConfig{
+			HTTPAddrs: mgmtEndpoints,
+			MemdAddrs: kvDataHosts,
+		},
+		CompressionConfig:  m.compressionConfig,
+		ConfigPollerConfig: m.configPollerConfig,
+		HTTPConfig:         m.httpConfig,
 		BucketName:         bucketName,
 	})
 }
@@ -278,150 +269,34 @@ func (m *BucketsTrackingAgentManager) GetClusterAgent(ctx context.Context) (*Age
 }
 
 func (m *BucketsTrackingAgentManager) GetBucketAgent(ctx context.Context, bucketName string) (*Agent, error) {
-	m.bucketsLock.Lock()
+	m.stateLock.Lock()
 	if m.closed {
-		m.bucketsLock.Unlock()
+		m.stateLock.Unlock()
 		return nil, errors.New("agent manager closed")
 	}
+	m.stateLock.Unlock()
 
-	agent, ok := m.bucketAgents[bucketName]
-	if ok {
-		m.bucketsLock.Unlock()
-		return agent, nil
-	}
-	m.bucketsLock.Unlock()
-
-	m.logger.Debug("Bucket unknown, attempting manual update", zap.String("name", bucketName))
-
-	// This will update buckets and close the needsBucketChan if successful
-	buckets, err := m.manuallyFetchBuckets(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	m.handleBuckets(buckets)
-
-	// We still don't know the bucket at this point then we can say it doesn't exist.
-	m.bucketsLock.Lock()
-	agent, ok = m.bucketAgents[bucketName]
-	m.bucketsLock.Unlock()
-	if !ok {
-		return nil, cbmgmtx.ErrBucketNotFound
-	}
-
-	return agent, nil
+	return m.bucketsWatcher.GetAgent(ctx, bucketName)
 }
 
 // Close closes the AgentManager and all underlying Agent instances.
 func (m *BucketsTrackingAgentManager) Close() error {
-	m.bucketsLock.Lock()
-	defer m.bucketsLock.Unlock()
+	m.logger.Debug("Closing")
 
 	firstErr := m.clusterAgent.Close()
-	for _, agent := range m.bucketAgents {
-		err := agent.Close()
-		if err != nil && firstErr == nil {
-			firstErr = err
-		}
+
+	err := m.bucketsWatcher.Close()
+	if err != nil && firstErr == nil {
+		firstErr = err
 	}
+
+	m.stateLock.Lock()
+	m.clusterAgent = nil
+	m.closed = true
+	m.state.httpTransport.CloseIdleConnections()
+	m.stateLock.Unlock()
 
 	m.watchersCancel()
 
-	m.state.httpTransport.CloseIdleConnections()
-
-	m.clusterAgent = nil
-	m.bucketAgents = nil
-	m.closed = true
-
 	return firstErr
-}
-
-func (m *BucketsTrackingAgentManager) manuallyFetchBuckets(ctx context.Context) ([]bucketDescriptor, error) {
-	var recentEndpoints []string
-	var latestErr error
-
-	for {
-		if ctx.Err() != nil {
-			return nil, ctx.Err()
-		}
-
-		m.stateLock.Lock()
-		state := m.state
-		m.stateLock.Unlock()
-
-		mgmtEndpoints := m.mgmtEndpoints()
-
-		// if there are no endpoints to poll, this is unexpected
-		if len(mgmtEndpoints) == 0 {
-			return nil, errors.New("manager has no endpoints to question for bucket existence")
-		}
-
-		// remove the endpoints that we've already used
-		remainingEndpoints := filterStringsOut(mgmtEndpoints, recentEndpoints)
-
-		// if there are no endpoints left, we must have an error so return it
-		if len(remainingEndpoints) == 0 {
-			return nil, latestErr
-		}
-
-		endpoint := remainingEndpoints[0]
-		recentEndpoints = append(recentEndpoints, endpoint)
-
-		buckets, err := bucketsTracker_fetchOneBuckets(
-			ctx,
-			m.logger,
-			state.httpTransport,
-			endpoint,
-			m.userAgent,
-			state.authenticator)
-		if err != nil {
-			latestErr = err
-			m.logger.Debug("failed to fetch config via http",
-				zap.Error(err),
-				zap.String("endpoint", endpoint))
-			continue
-		}
-
-		return buckets, nil
-	}
-}
-
-func bucketsTracker_fetchOneBuckets(
-	ctx context.Context,
-	logger *zap.Logger,
-	httpRoundTripper http.RoundTripper,
-	endpoint string,
-	userAgent string,
-	authenticator Authenticator,
-) ([]bucketDescriptor, error) {
-	host, err := getHostFromUri(endpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	username, password, err := authenticator.GetCredentials(ServiceTypeMgmt, host)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := cbmgmtx.Management{
-		Transport: httpRoundTripper,
-		UserAgent: userAgent,
-		Endpoint:  endpoint,
-		Username:  username,
-		Password:  password,
-	}.GetClusterConfig(ctx, &cbmgmtx.GetClusterConfigOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	buckets := make([]bucketDescriptor, len(resp.BucketNames))
-	for i, b := range resp.BucketNames {
-		buckets[i] = bucketDescriptor{
-			Name: b.BucketName,
-			UUID: b.UUID,
-		}
-	}
-
-	return buckets, nil
 }
---
+++ bucketswatcher_http.go
@@ -0,0 +1,363 @@
+package gocbcorex
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/couchbase/gocbcorex/cbmgmtx"
+
+	"golang.org/x/exp/slices"
+
+	"go.uber.org/zap"
+)
+
+type BucketsWatcherHttpConfig struct {
+	HttpRoundTripper http.RoundTripper
+	Endpoints        []string
+	UserAgent        string
+	Authenticator    Authenticator
+
+	MakeAgent func(ctx context.Context, bucketName string) (*Agent, error)
+}
+
+type BucketsWatcherHttpReconfigureConfig struct {
+	HttpRoundTripper http.RoundTripper
+	Endpoints        []string
+	Authenticator    Authenticator
+}
+
+type BucketsWatcherHttpOptions struct {
+	Logger               *zap.Logger
+	AutoDisconnectPeriod time.Duration
+}
+
+type bucketsWatcherHttpState struct {
+	authenticator    Authenticator
+	httpRoundTripper http.RoundTripper
+	endpoints        []string
+}
+
+type BucketsWatcherHttp struct {
+	stateLock sync.Mutex
+	state     *bucketsWatcherHttpState
+	logger    *zap.Logger
+	userAgent string
+	closed    bool
+
+	agents    map[string]*Agent
+	lock      sync.Mutex
+	makeAgent func(ctx context.Context, bucketName string) (*Agent, error)
+
+	watcher   *StreamWatcherHttp[[]bucketDescriptor]
+	closedSig chan struct{}
+
+	needsBucketChan       chan struct{}
+	lastUpdateTriggeredAt time.Time
+	updateSig             chan struct{}
+}
+
+func NewBucketsWatcherHttp(cfg BucketsWatcherHttpConfig, opts BucketsWatcherHttpOptions) (*BucketsWatcherHttp, error) {
+	logger := loggerOrNop(opts.Logger)
+
+	watcher, err := NewStreamWatcherHttp[[]bucketDescriptor](&StreamWatcherHttpConfig{
+		HttpRoundTripper: cfg.HttpRoundTripper,
+		Endpoints:        cfg.Endpoints,
+		UserAgent:        cfg.UserAgent,
+		Authenticator:    cfg.Authenticator,
+	}, &StreamWatcherHttpOptions{
+		Logger:               logger.Named("buckets-stream"),
+		AutoDisconnectPeriod: opts.AutoDisconnectPeriod,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	bw := &BucketsWatcherHttp{
+		state: &bucketsWatcherHttpState{
+			authenticator:    cfg.Authenticator,
+			httpRoundTripper: cfg.HttpRoundTripper,
+			endpoints:        cfg.Endpoints,
+		},
+
+		makeAgent: cfg.MakeAgent,
+		userAgent: cfg.UserAgent,
+		logger:    logger,
+		agents:    make(map[string]*Agent),
+		watcher:   watcher,
+		closedSig: make(chan struct{}),
+		updateSig: make(chan struct{}, 1),
+	}
+
+	go bw.manualUpdateThread()
+
+	return bw, nil
+}
+
+func (w *BucketsWatcherHttp) Reconfigure(cfg *BucketsWatcherHttpReconfigureConfig) error {
+	w.stateLock.Lock()
+	w.state = &bucketsWatcherHttpState{
+		httpRoundTripper: cfg.HttpRoundTripper,
+		endpoints:        cfg.Endpoints,
+		authenticator:    cfg.Authenticator,
+	}
+	w.stateLock.Unlock()
+
+	w.watcher.Reconfigure(&StreamWatcherHttpReconfigureConfig{
+		HttpRoundTripper: cfg.HttpRoundTripper,
+		Endpoints:        cfg.Endpoints,
+		Authenticator:    cfg.Authenticator,
+	})
+
+	return nil
+}
+
+func (bw *BucketsWatcherHttp) manualUpdateThread() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-bw.closedSig
+		cancel()
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-bw.updateSig:
+		}
+
+		buckets, err := bw.manuallyFetchBuckets(ctx)
+		if err != nil {
+			bw.logger.Info("Failed to manually fetch buckets", zap.Error(err))
+			continue
+		}
+
+		bw.handleBuckets(ctx, buckets)
+
+		bw.lock.Lock()
+		if bw.needsBucketChan != nil {
+			close(bw.needsBucketChan)
+			bw.needsBucketChan = nil
+		}
+		bw.lock.Unlock()
+	}
+}
+
+func (bw *BucketsWatcherHttp) GetAgent(ctx context.Context, bucketName string) (*Agent, error) {
+	getAgentMadeAt := time.Now()
+
+	for {
+		bw.lock.Lock()
+		if bw.closed {
+			bw.lock.Unlock()
+			return nil, errors.New("buckets watcher closed")
+		}
+
+		agent, ok := bw.agents[bucketName]
+		if ok {
+			bw.lock.Unlock()
+			return agent, nil
+		}
+
+		var lastUpdateTriggeredAt time.Time
+		if bw.needsBucketChan == nil {
+			bw.needsBucketChan = make(chan struct{})
+			lastUpdateTriggeredAt = time.Now()
+			bw.lastUpdateTriggeredAt = lastUpdateTriggeredAt
+
+			bw.logger.Debug("Bucket unknown, triggering manual update", zap.String("name", bucketName))
+
+			bw.updateSig <- struct{}{}
+		} else {
+			bw.logger.Debug("Bucket unknown, manual update already in progress", zap.String("name", bucketName))
+		}
+
+		needsBucketChan := bw.needsBucketChan
+		lastUpdateTriggeredAt = bw.lastUpdateTriggeredAt
+		bw.lock.Unlock()
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-needsBucketChan:
+		}
+
+		// We still don't know the bucket at this point then we can say it doesn't exist.
+		bw.lock.Lock()
+		agent, ok = bw.agents[bucketName]
+		bw.lock.Unlock()
+		if !ok {
+			if lastUpdateTriggeredAt.Before(getAgentMadeAt) {
+				bw.logger.Debug("Bucket still unknown, but update after request was made - retrying", zap.String("name", bucketName))
+				continue
+			}
+			bw.logger.Debug("Bucket still unknown, erroring", zap.String("name", bucketName))
+			return nil, cbmgmtx.ErrBucketNotFound
+		}
+
+		return agent, nil
+	}
+}
+
+func (bw *BucketsWatcherHttp) Watch() {
+	ctx, cancel := context.WithCancel(context.Background())
+	bucketCh := bw.watcher.Watch(ctx, streamWatcherHttp_streamBuckets)
+
+	go func() {
+		// Cancel the context and wait for the configCh to be closed.
+		<-bw.closedSig
+		cancel()
+	}()
+
+	go func() {
+		for buckets := range bucketCh {
+			bw.handleBuckets(ctx, buckets)
+		}
+	}()
+}
+
+func (bw *BucketsWatcherHttp) Close() error {
+	bw.stateLock.Lock()
+	defer bw.stateLock.Unlock()
+
+	bw.logger.Debug("Closing")
+
+	bw.closed = true
+
+	close(bw.closedSig)
+
+	var firstErr error
+	for _, agent := range bw.agents {
+		err := agent.Close()
+		if err != nil && firstErr == nil {
+			firstErr = err
+		}
+	}
+
+	bw.logger.Debug("Closed")
+
+	bw.agents = nil
+
+	return firstErr
+}
+
+func (bw *BucketsWatcherHttp) handleBuckets(ctx context.Context, buckets []bucketDescriptor) {
+	bw.lock.Lock()
+	defer bw.lock.Unlock()
+
+	for _, bucket := range buckets {
+		if _, ok := bw.agents[bucket.Name]; !ok {
+			bw.logger.Debug("New bucket on cluster, creating agent", zap.String("name", bucket.Name))
+			agent, err := bw.makeAgent(ctx, bucket.Name)
+			if err != nil {
+				bw.logger.Debug("Failed to create agent", zap.String("name", bucket.Name))
+				continue
+			}
+
+			bw.agents[bucket.Name] = agent
+		}
+	}
+
+	for bucket, agent := range bw.agents {
+		if !slices.ContainsFunc(buckets, func(descriptor bucketDescriptor) bool {
+			return descriptor.Name == bucket
+		}) {
+			bw.logger.Debug("Bucket no longer on cluster, shutting down agent", zap.String("name", bucket))
+			delete(bw.agents, bucket)
+			err := agent.Close()
+			if err != nil {
+				bw.logger.Debug("Failed to close agent", zap.String("name", bucket))
+			}
+		}
+	}
+}
+
+func (bw *BucketsWatcherHttp) manuallyFetchBuckets(ctx context.Context) ([]bucketDescriptor, error) {
+	var recentEndpoints []string
+	var latestErr error
+
+	for {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+
+		bw.stateLock.Lock()
+		state := bw.state
+		bw.stateLock.Unlock()
+
+		// if there are no endpoints to poll, this is unexpected
+		if len(state.endpoints) == 0 {
+			return nil, errors.New("watcher has no endpoints to question for bucket existence")
+		}
+
+		// remove the endpoints that we've already used
+		remainingEndpoints := filterStringsOut(state.endpoints, recentEndpoints)
+
+		// if there are no endpoints left, we must have an error so return it
+		if len(remainingEndpoints) == 0 {
+			return nil, latestErr
+		}
+
+		endpoint := remainingEndpoints[0]
+		recentEndpoints = append(recentEndpoints, endpoint)
+
+		buckets, err := bucketsTracker_fetchOneBuckets(
+			ctx,
+			bw.logger,
+			state.httpRoundTripper,
+			endpoint,
+			bw.userAgent,
+			state.authenticator)
+		if err != nil {
+			latestErr = err
+			bw.logger.Debug("failed to fetch buckets via http",
+				zap.Error(err),
+				zap.String("endpoint", endpoint))
+			continue
+		}
+
+		return buckets, nil
+	}
+}
+
+func bucketsTracker_fetchOneBuckets(
+	ctx context.Context,
+	logger *zap.Logger,
+	httpRoundTripper http.RoundTripper,
+	endpoint string,
+	userAgent string,
+	authenticator Authenticator,
+) ([]bucketDescriptor, error) {
+	host, err := getHostFromUri(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	username, password, err := authenticator.GetCredentials(ServiceTypeMgmt, host)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := cbmgmtx.Management{
+		Transport: httpRoundTripper,
+		UserAgent: userAgent,
+		Endpoint:  endpoint,
+		Username:  username,
+		Password:  password,
+	}.GetClusterConfig(ctx, &cbmgmtx.GetClusterConfigOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	buckets := make([]bucketDescriptor, len(resp.BucketNames))
+	for i, b := range resp.BucketNames {
+		buckets[i] = bucketDescriptor{
+			Name: b.BucketName,
+			UUID: b.UUID,
+		}
+	}
+
+	return buckets, nil
+}
