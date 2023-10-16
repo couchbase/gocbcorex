c9728d6b91f7a55bea149b58634a694b0016fb57
Charles Dixon <chvckd@gmail.com>
2023-10-16 11:58:20 +0100

    ING-598: Add handling for index exists/not found to cbqueryx
    Motivation
    ----------
    We need to expose index exists and not found from cbqueryx, and
    add handling for their respective error codes. This will enable
    users to be able to handle these errors without having to manually
    parse the error that we return.

--- cbqueryx/query_test.go
+++ cbqueryx/query_test.go
@@ -4,9 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/google/uuid"
 
 	"github.com/couchbase/gocbcorex/testutils"
 	"github.com/stretchr/testify/require"
@@ -44,4 +49,74 @@ func TestQuery(t *testing.T) {
 	assertQueryResult(t, expectedRows, &expectedResult, res)
 
 	require.Empty(t, cache.queryCache)
+}
+
+func TestQueryIndexExists(t *testing.T) {
+	index := uuid.NewString()[:6]
+	expectedResult := makeErrorQueryResult([]queryErrorJson{
+		{
+			Code: 4300,
+			Msg:  fmt.Sprintf("The index %s already exists.", index),
+			Reason: map[string]interface{}{
+				"name": index,
+			},
+		},
+	})
+	body, err := json.Marshal(expectedResult)
+	require.NoError(t, err)
+
+	resp := &http.Response{
+		Status:        "conflict",
+		StatusCode:    409,
+		Header:        nil,
+		Body:          io.NopCloser(bytes.NewReader(body)),
+		ContentLength: int64(len(body)),
+	}
+
+	opts := &QueryOptions{
+		Statement: fmt.Sprintf("CREATE INDEX %s", index),
+	}
+	_, err = Query{
+		Transport: makeSingleTestRoundTripper(resp, nil),
+		Logger:    testutils.MakeTestLogger(t),
+		UserAgent: "useragent",
+		Username:  "username",
+		Password:  "password",
+	}.Query(context.Background(), opts)
+	assert.ErrorIs(t, err, ErrIndexExists)
+}
+
+func TestQueryIndexNotFound(t *testing.T) {
+	index := uuid.NewString()[:6]
+	expectedResult := makeErrorQueryResult([]queryErrorJson{
+		{
+			Code: 12016,
+			Msg:  fmt.Sprintf("Index Not Found - cause: GSI index %s not found.", index),
+			Reason: map[string]interface{}{
+				"name": index,
+			},
+		},
+	})
+	body, err := json.Marshal(expectedResult)
+	require.NoError(t, err)
+
+	resp := &http.Response{
+		Status:        "success",
+		StatusCode:    200,
+		Header:        nil,
+		Body:          io.NopCloser(bytes.NewReader(body)),
+		ContentLength: int64(len(body)),
+	}
+
+	opts := &QueryOptions{
+		Statement: fmt.Sprintf("CREATE INDEX %s", index),
+	}
+	_, err = Query{
+		Transport: makeSingleTestRoundTripper(resp, nil),
+		Logger:    testutils.MakeTestLogger(t),
+		UserAgent: "useragent",
+		Username:  "username",
+		Password:  "password",
+	}.Query(context.Background(), opts)
+	assert.ErrorIs(t, err, ErrIndexNotFound)
 }
