2bf3d07896273ca0dac5237b20850615ec16cc88
Charles Dixon <chvckd@gmail.com>
2023-10-31 14:57:42 +0000

    ING_622: Update scanwait and timeout to send values as go strings

--- cbqueryx/query_options_test.go
+++ cbqueryx/query_options_test.go
@@ -2,17 +2,64 @@ package cbqueryx
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEncodeQueryOptions(t *testing.T) {
-	opts := &Options{
-		Statement: "SELECT *",
-	}
+	t.Run("statement", func(t *testing.T) {
+		opts := &Options{
+			Statement: "SELECT *",
+		}
 
-	optsJson, err := opts.encodeToJson()
-	assert.NoError(t, err)
+		optsJson, err := opts.encodeToJson()
+		assert.NoError(t, err)
 
-	assert.Equal(t, `{"statement":"SELECT *"}`, string(optsJson))
+		assert.Equal(t, `{"statement":"SELECT *"}`, string(optsJson))
+	})
+
+	t.Run("scan_wait", func(t *testing.T) {
+		opts := &Options{
+			ScanWait: 2500 * time.Millisecond,
+		}
+
+		optsJson, err := opts.encodeToJson()
+		assert.NoError(t, err)
+
+		assert.Equal(t, `{"scan_wait":"2.5s"}`, string(optsJson))
+	})
+
+	t.Run("timeout", func(t *testing.T) {
+		opts := &Options{
+			Timeout: 2500 * time.Millisecond,
+		}
+
+		optsJson, err := opts.encodeToJson()
+		assert.NoError(t, err)
+
+		assert.Equal(t, `{"timeout":"2.5s"}`, string(optsJson))
+	})
+
+	t.Run("tx_timeout", func(t *testing.T) {
+		opts := &Options{
+			TxTimeout: 2500 * time.Millisecond,
+		}
+
+		optsJson, err := opts.encodeToJson()
+		assert.NoError(t, err)
+
+		assert.Equal(t, `{"txtimeout":"2.5s"}`, string(optsJson))
+	})
+
+	t.Run("kvtimeout", func(t *testing.T) {
+		opts := &Options{
+			KvTimeout: 2500 * time.Millisecond,
+		}
+
+		optsJson, err := opts.encodeToJson()
+		assert.NoError(t, err)
+
+		assert.Equal(t, `{"kvtimeout":"2.5s"}`, string(optsJson))
+	})
 }
