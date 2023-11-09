20328814fbb0a625f0483d1a91c5886aff12a06b
Brett Lawson <brett19@gmail.com>
2023-11-08 17:42:59 -0800

    ING-627: Fixed GetClusterConfig to return semantic error.

--- memdx/ops_core.go
+++ memdx/ops_core.go
@@ -183,7 +183,7 @@ func (o OpsCore) GetClusterConfig(d Dispatcher, req *GetClusterConfigRequest, cb
 			// ns_server has not posted a configuration for the bucket to kv_engine yet. We
 			// transform this into a ErrTmpFail as we make the assumption that the
 			// SelectBucket will have failed if this was anything but a transient issue.
-			cb(nil, ErrTmpFail)
+			cb(nil, ErrConfigNotSet)
 			return false
 		}
 
