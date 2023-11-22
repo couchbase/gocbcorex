a6cd81b84a3297ba1581546722689680d35d84f4
Charles Dixon <chvckd@gmail.com>
2023-11-22 09:18:22 +0000

    Add logging for agent config on agent startup

--- agent_options.go
+++ agent_options.go
@@ -2,7 +2,10 @@ package gocbcorex
 
 import (
 	"crypto/tls"
+	"strings"
 	"time"
+
+	"go.uber.org/zap/zapcore"
 
 	"go.uber.org/zap"
 )
@@ -32,10 +35,35 @@ type AgentOptions struct {
 	HTTPConfig HTTPConfig
 }
 
+func (opts AgentOptions) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("bucket-name", opts.BucketName)
+	if err := enc.AddObject("seed-config", opts.SeedConfig); err != nil {
+		return err
+	}
+	if err := enc.AddObject("compression-config", opts.CompressionConfig); err != nil {
+		return err
+	}
+	if err := enc.AddObject("poller-config", opts.ConfigPollerConfig); err != nil {
+		return err
+	}
+	if err := enc.AddObject("http-config", opts.HTTPConfig); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // SeedConfig specifies initial seed configuration options such as addresses.
 type SeedConfig struct {
 	HTTPAddrs []string
 	MemdAddrs []string
+}
+
+func (c SeedConfig) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("http-addresses", strings.Join(c.HTTPAddrs, ","))
+	enc.AddString("memd-addresses", strings.Join(c.MemdAddrs, ","))
+
+	return nil
 }
 
 // CompressionConfig specifies options for controlling compression applied to documents using KV.
@@ -46,6 +74,15 @@ type CompressionConfig struct {
 	MinRatio             float64
 }
 
+func (c CompressionConfig) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddBool("enabled", c.EnableCompression)
+	enc.AddBool("decompression-disabled", c.DisableDecompression)
+	enc.AddInt("min-size", c.MinSize)
+	enc.AddFloat64("min-ratio", c.MinRatio)
+
+	return nil
+}
+
 // ConfigPollerConfig specifies options for controlling the cluster configuration pollers.
 type ConfigPollerConfig struct {
 	HTTPRedialPeriod time.Duration
@@ -53,6 +90,14 @@ type ConfigPollerConfig struct {
 	HTTPMaxWait      time.Duration
 	// CccpMaxWait      time.Duration
 	// CccpPollPeriod   time.Duration
+}
+
+func (c ConfigPollerConfig) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("redial-period", c.HTTPRedialPeriod.String())
+	enc.AddString("retry-delay", c.HTTPRetryDelay.String())
+	enc.AddString("max-wait", c.HTTPMaxWait.String())
+
+	return nil
 }
 
 // HTTPConfig specifies http related configuration options.
@@ -65,4 +110,13 @@ type HTTPConfig struct {
 	// IdleConnectionTimeout is the maximum amount of time an idle (keep-alive) connection will remain idle before
 	// closing itself.
 	IdleConnectionTimeout time.Duration
+}
+
+func (c HTTPConfig) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddInt("max-idle-conns", c.MaxIdleConns)
+	enc.AddInt("max-idle-conns-per-host", c.MaxIdleConnsPerHost)
+	enc.AddString("connection-timeout", c.ConnectTimeout.String())
+	enc.AddString("idle-connection-timeout", c.IdleConnectionTimeout.String())
+
+	return nil
 }
