5d977a9324fefe8f4f02bb9b06d4e3b667cb7716
Brett Lawson <unknown>
2024-02-12 13:59:10 -0800

    ING-708: Added dino-testing for EnsureManifest.

--- cbmgmtx/ensuremanifesthelper_int_test.go
+++ cbmgmtx/ensuremanifesthelper_int_test.go
@@ -2,6 +2,7 @@ package cbmgmtx_test
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strconv"
 	"testing"
@@ -14,16 +15,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestEnsureManifest(t *testing.T) {
-	testutilsint.SkipIfShortTest(t)
+func TestEnsureManifestDino(t *testing.T) {
+	testutilsint.SkipIfNoDinoCluster(t)
 
 	ctx := context.Background()
+	transport := http.DefaultTransport
 	bucketName := testutilsint.TestOpts.BucketName
 	scopeName := "_default"
 	testCollectionName := "testscope-" + uuid.NewString()[:6]
-	transport := http.DefaultTransport
 
 	nodes := testutilsint.GetTestNodes(t)
+
+	blockNode := nodes.SelectFirst(t, func(node *testutilsint.NodeTarget) bool {
+		return !node.IsOrchestrator
+	})
+	execNode := nodes.SelectLast(t, func(node *testutilsint.NodeTarget) bool {
+		return node != blockNode
+	})
+
+	blockHost := blockNode.Hostname
+	execEndpoint := execNode.NsEndpoint()
+
+	log.Printf("nodes:")
+	for _, node := range nodes {
+		log.Printf("  %s", node)
+	}
+	log.Printf("execution endpoint: %s", execEndpoint)
+	log.Printf("blocked host: %s", blockHost)
 
 	var targets []cbmgmtx.NodeTarget
 	for _, node := range nodes {
@@ -34,48 +52,129 @@ func TestEnsureManifest(t *testing.T) {
 		})
 	}
 
-	// we intentionally use the last target that will be polled as the node
-	// to create the bucket with so we don't unintentionally give additional
-	// time for nodes to sync their configuration
 	mgmt := cbmgmtx.Management{
 		Transport: transport,
 		UserAgent: "useragent",
-		Endpoint:  targets[len(targets)-1].Endpoint,
+		Endpoint:  execEndpoint,
 		Username:  testutilsint.TestOpts.Username,
 		Password:  testutilsint.TestOpts.Password,
 	}
 
-	createResp, err := mgmt.CreateCollection(ctx, &cbmgmtx.CreateCollectionOptions{
-		BucketName:     bucketName,
-		ScopeName:      scopeName,
-		CollectionName: testCollectionName,
-		MaxTTL:         0,
-	})
-	require.NoError(t, err)
+	createTestCollection := func() uint64 {
+		var manifestUid uint64
+		require.Eventually(t, func() bool {
+			log.Printf("attempting to create the collection")
+			createResp, err := mgmt.CreateCollection(ctx, &cbmgmtx.CreateCollectionOptions{
+				BucketName:     bucketName,
+				ScopeName:      scopeName,
+				CollectionName: testCollectionName,
+				MaxTTL:         0,
+			})
+			if err != nil {
+				log.Printf("collection creation failed with error: %s", err)
+				return false
+			}
 
-	manifestUid, _ := strconv.ParseUint(createResp.ManifestUid, 16, 64)
+			manifestUid, _ = strconv.ParseUint(createResp.ManifestUid, 16, 64)
 
-	t.Cleanup(func() {
-		_, err = mgmt.DeleteCollection(ctx, &cbmgmtx.DeleteCollectionOptions{
-			BucketName:     bucketName,
-			ScopeName:      scopeName,
-			CollectionName: testCollectionName,
-			OnBehalfOf:     nil,
-		})
-		require.NoError(t, err)
-	})
+			return true
+		}, 120*time.Second, 1*time.Second)
+
+		return manifestUid
+	}
+
+	deleteTestCollection := func() uint64 {
+		var manifestUid uint64
+		require.Eventually(t, func() bool {
+			log.Printf("attempting to delete the collection")
+			resp, err := mgmt.DeleteCollection(ctx, &cbmgmtx.DeleteCollectionOptions{
+				BucketName:     bucketName,
+				ScopeName:      scopeName,
+				CollectionName: testCollectionName,
+				OnBehalfOf:     nil,
+			})
+			if err != nil {
+				log.Printf("collection deletion failed with error: %s", err)
+				return false
+			}
+
+			manifestUid, _ = strconv.ParseUint(resp.ManifestUid, 16, 64)
+
+			return true
+		}, 120*time.Second, 1*time.Second)
+
+		return manifestUid
+	}
+
+	// start dino testing
+	dino := testutilsint.StartDinoTesting(t, true)
+
+	// block access to the first endpoint
+	dino.BlockNodeTraffic(blockHost)
+
+	manifestUid := createTestCollection()
 
 	hlpr := cbmgmtx.EnsureManifestHelper{
 		Logger:     testutils.MakeTestLogger(t),
 		UserAgent:  "useragent",
 		OnBehalfOf: nil,
 
-		BucketName:  "default",
+		BucketName:  bucketName,
 		ManifestUid: manifestUid,
 	}
 
+	require.Never(t, func() bool {
+		res, err := hlpr.Poll(ctx, &cbmgmtx.EnsureManifestPollOptions{
+			Transport: transport,
+			Targets:   targets,
+		})
+		require.NoError(t, err)
+
+		return res
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// stop blocking traffic to the node
+	dino.AllowTraffic(blockHost)
+
 	require.Eventually(t, func() bool {
 		res, err := hlpr.Poll(ctx, &cbmgmtx.EnsureManifestPollOptions{
+			Transport: transport,
+			Targets:   targets,
+		})
+		require.NoError(t, err)
+
+		return res
+	}, 30*time.Second, 500*time.Millisecond)
+
+	// now lets block traffic again before we delete
+	dino.BlockNodeTraffic(blockHost)
+
+	// delete the bucket
+	manifestUid = deleteTestCollection()
+
+	hlprOut := cbmgmtx.EnsureManifestHelper{
+		Logger:    testutils.MakeTestLogger(t),
+		UserAgent: "useragent",
+
+		BucketName:  bucketName,
+		ManifestUid: manifestUid,
+	}
+
+	require.Never(t, func() bool {
+		res, err := hlprOut.Poll(ctx, &cbmgmtx.EnsureManifestPollOptions{
+			Transport: transport,
+			Targets:   targets,
+		})
+		require.NoError(t, err)
+
+		return res
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// stop blocking traffic to the node
+	dino.AllowTraffic(blockHost)
+
+	require.Eventually(t, func() bool {
+		res, err := hlprOut.Poll(ctx, &cbmgmtx.EnsureManifestPollOptions{
 			Transport: transport,
 			Targets:   targets,
 		})
