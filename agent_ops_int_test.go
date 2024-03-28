679ef5b030d8bfa6fab28e407fe85fb8ff688bd3
Brett Lawson <>
2024-03-28 11:29:25 -0700

    ING-781: Do not panic getting server duration from nil responses.

--- agent_ops_int_test.go
+++ agent_ops_int_test.go
@@ -12,6 +12,9 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"go.uber.org/zap"
 )
 
@@ -103,4 +106,93 @@ func TestAgentDoesNotRetryMemdxInvalidArgs(t *testing.T) {
 		VbucketID: 1,
 	})
 	require.ErrorIs(t, err, memdx.ErrInvalidArgument)
+}
+
+func TestServerDurations(t *testing.T) {
+	testutilsint.SkipIfShortTest(t)
+
+	logger, _ := zap.NewDevelopment()
+
+	opts := gocbcorex.AgentOptions{
+		Logger:    logger,
+		TLSConfig: nil,
+		Authenticator: &gocbcorex.PasswordAuthenticator{
+			Username: testutilsint.TestOpts.Username,
+			Password: testutilsint.TestOpts.Password,
+		},
+		BucketName: testutilsint.TestOpts.BucketName,
+		SeedConfig: gocbcorex.SeedConfig{
+			HTTPAddrs: testutilsint.TestOpts.HTTPAddrs,
+			MemdAddrs: testutilsint.TestOpts.MemdAddrs,
+		},
+	}
+
+	agent, err := gocbcorex.CreateAgent(context.Background(), opts)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := agent.Close()
+		require.NoError(t, err)
+		require.False(t, leakcheck.ReportLeakedGoroutines())
+	})
+
+	docKey := uuid.NewString()
+
+	ctx := context.Background()
+
+	memExporter := tracetest.NewInMemoryExporter()
+	memTracer := trace.NewTracerProvider(
+		trace.WithSyncer(memExporter),
+	)
+	otel.SetTracerProvider(memTracer)
+
+	tracer := otel.Tracer("github.com/couchbase/gocbcorex.tests")
+	spanCtx, span := tracer.Start(ctx, "testGet")
+
+	memExporter.Reset()
+
+	// do an upsert, and then verify the server duration is included
+	upsertRes, err := agent.Upsert(spanCtx, &gocbcorex.UpsertOptions{
+		Key:            []byte(docKey),
+		ScopeName:      "",
+		CollectionName: "",
+		Value:          []byte(`{"foo": "bar"}`),
+	})
+	require.NoError(t, err)
+	assert.NotZero(t, upsertRes.Cas)
+
+	spans := memExporter.GetSpans()
+	spansSnap := spans.Snapshots()
+
+	foundServerDuration := false
+	for _, spanSnap := range spansSnap {
+		attribs := spanSnap.Attributes()
+		for _, attrib := range attribs {
+			if attrib.Key == "db.couchbase.server_duration" {
+				foundServerDuration = true
+			}
+		}
+	}
+	require.True(t, foundServerDuration)
+
+	// cleanup the document we created
+	deleteRes, err := agent.Delete(spanCtx, &gocbcorex.DeleteOptions{
+		Key:            []byte(docKey),
+		ScopeName:      "",
+		CollectionName: "",
+	})
+	require.NoError(t, err)
+	assert.NotZero(t, deleteRes.Cas)
+
+	// try to fetch the document again to trigger an error and confirm
+	// that we don't fail.  This is a regression test for ING-781.
+	_, err = agent.Get(spanCtx, &gocbcorex.GetOptions{
+		Key:            []byte(docKey),
+		ScopeName:      "",
+		CollectionName: "",
+	})
+	require.ErrorIs(t, err, memdx.ErrDocNotFound)
+
+	span.End()
+
+	otel.SetTracerProvider(nil)
 }
--- go.mod
+++ go.mod
@@ -7,9 +7,10 @@ require (
 	github.com/golang/snappy v0.0.4
 	github.com/google/go-querystring v1.1.0
 	github.com/google/uuid v1.3.0
-	github.com/stretchr/testify v1.8.3
-	go.opentelemetry.io/otel v1.16.0
-	go.opentelemetry.io/otel/trace v1.16.0
+	github.com/stretchr/testify v1.8.4
+	go.opentelemetry.io/otel v1.24.0
+	go.opentelemetry.io/otel/sdk v1.24.0
+	go.opentelemetry.io/otel/trace v1.24.0
 	go.uber.org/zap v1.24.0
 	golang.org/x/exp v0.0.0-20230131160201-f062dba9d201
 	golang.org/x/mod v0.14.0
@@ -17,12 +18,13 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
