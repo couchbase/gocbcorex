b5008d2482a49effafea6c51ad88eaa2825abbe1
Brett Lawson <>
2024-06-27 14:09:29 -0700

    Updated transactionsx file naming to match better with codebase.

--- transactionsx/errors.go
+++ transactionsx/errors.go
@@ -291,3 +291,23 @@ type ErrorClassError struct {
 func (e ErrorClassError) Error() string {
 	return fmt.Sprintf("error class error - %d", e.Class)
 }
+
+type wrappedError struct {
+	Message    string
+	InnerError error
+}
+
+func (e wrappedError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Message, e.InnerError.Error())
+}
+
+func (e wrappedError) Unwrap() error {
+	return e.InnerError
+}
+
+func wrapError(err error, message string) error {
+	return wrappedError{
+		Message:    message,
+		InnerError: err,
+	}
+}
