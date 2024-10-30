4a57284d10f80304a79db4f02af88d2082a156a3
Brett Lawson <>
2024-10-30 16:05:20 -0700

    ING-704: Added initial DCP implementation to memdx.

--- memdx/unsolicited_events.go
+++ memdx/unsolicited_events.go
@@ -6,3 +6,151 @@ type ClustermapChangeEvent struct {
 	Rev        int64
 	Config     []byte
 }
+
+type DcpSnapshotState uint32
+
+const (
+	DcpSnapshotStateInMemory         DcpSnapshotState = 1 << 0
+	DcpSnapshotStateOnDisk           DcpSnapshotState = 1 << 1
+	DcpSnapshotStateHistory          DcpSnapshotState = 1 << 4
+	DcpSnapshotStateMayDuplicateKeys DcpSnapshotState = 1 << 5
+)
+
+type DcpSnapshotMarkerEvent struct {
+	StreamId           uint16
+	Version            int
+	StartSeqNo         uint64
+	EndSeqNo           uint64
+	VbucketId          uint16
+	SnapshotType       DcpSnapshotState
+	MaxVisibleSeqNo    uint64
+	HighCompletedSeqNo uint64
+	SnapshotTimeStamp  uint64
+}
+
+type DcpMutationEvent struct {
+	StreamId     uint16
+	Version      int
+	SeqNo        uint64
+	RevNo        uint64
+	Cas          uint64
+	Flags        uint32
+	Expiry       uint32
+	LockTime     uint32
+	CollectionId uint32
+	VbucketId    uint16
+	Datatype     uint8
+	Key          []byte
+	Value        []byte
+	MetaDataSize uint16
+	NRU          uint8
+}
+
+type DcpDeletionEvent struct {
+	StreamId     uint16
+	Version      int
+	SeqNo        uint64
+	RevNo        uint64
+	Cas          uint64
+	DeleteTime   uint32
+	CollectionId uint32
+	VbucketId    uint16
+	Datatype     uint8
+	Key          []byte
+}
+
+type DcpExpirationEvent struct {
+	StreamId     uint16
+	Version      int
+	SeqNo        uint64
+	RevNo        uint64
+	Cas          uint64
+	DeleteTime   uint32
+	CollectionId uint32
+	VbucketId    uint16
+	Datatype     uint8
+	Key          []byte
+}
+
+type DcpCollectionCreationEvent struct {
+	StreamId       uint16
+	SeqNo          uint64
+	Version        uint8
+	VbucketId      uint16
+	ManifestUid    uint64
+	ScopeId        uint32
+	CollectionId   uint32
+	Ttl            uint32
+	CollectionName string
+}
+
+type DcpCollectionDeletionEvent struct {
+	StreamId     uint16
+	Version      uint8
+	SeqNo        uint64
+	ManifestUid  uint64
+	ScopeID      uint32
+	CollectionId uint32
+	VbucketId    uint16
+}
+
+type DcpCollectionFlushEvent struct {
+	StreamId     uint16
+	Version      uint8
+	SeqNo        uint64
+	VbucketId    uint16
+	ManifestUid  uint64
+	CollectionId uint32
+}
+
+type DcpScopeCreationEvent struct {
+	StreamId    uint16
+	Version     uint8
+	SeqNo       uint64
+	VbucketId   uint16
+	ManifestUid uint64
+	ScopeId     uint32
+	ScopeName   string
+}
+
+type DcpScopeDeletionEvent struct {
+	StreamId    uint16
+	Version     uint8
+	SeqNo       uint64
+	VbucketId   uint16
+	ManifestUid uint64
+	ScopeId     uint32
+}
+
+type DcpCollectionModificationEvent struct {
+	StreamId     uint16
+	Version      uint8
+	SeqNo        uint64
+	ManifestUid  uint64
+	CollectionId uint32
+	Ttl          uint32
+	VbucketId    uint16
+}
+
+type DcpStreamEndEvent struct {
+	StreamId  uint16
+	VbucketId uint16
+}
+
+type DcpOSOSnapshotEvent struct {
+	StreamId     uint16
+	SnapshotType uint32
+	VbucketId    uint16
+}
+
+type DcpSeqNoAdvancedEvent struct {
+	StreamId  uint16
+	SeqNo     uint64
+	VbucketId uint16
+}
+
+type DcpNoOpEvent struct {
+}
+
+type DcpNoOpEventResponse struct {
+}
--- memdx/unsolicited_ops.go
+++ memdx/unsolicited_ops.go
@@ -6,11 +6,424 @@ import (
 	"fmt"
 )
 
+type Replier interface {
+	WritePacket(*Packet) error
+}
+
 type UnsolicitedOpsHandlers struct {
 	ClustermapChange func(req *ClustermapChangeEvent) error
+
+	DcpSnapshotMarker     func(req *DcpSnapshotMarkerEvent) error
+	DcpMutation           func(req *DcpMutationEvent) error
+	DcpDeletion           func(req *DcpDeletionEvent) error
+	DcpExpiration         func(req *DcpExpirationEvent) error
+	DcpCollectionCreation func(req *DcpCollectionCreationEvent) error
+	DcpCollectionDeletion func(req *DcpCollectionDeletionEvent) error
+	DcpCollectionFlush    func(req *DcpCollectionFlushEvent) error
+	DcpScopeCreation      func(req *DcpScopeCreationEvent) error
+	DcpScopeDeletion      func(req *DcpScopeDeletionEvent) error
+	DcpCollectionChanged  func(req *DcpCollectionModificationEvent) error
+	DcpStreamEnd          func(req *DcpStreamEndEvent) error
+	DcpOSOSnapshot        func(req *DcpOSOSnapshotEvent) error
+	DcpSeqNoAdvanced      func(req *DcpSeqNoAdvancedEvent) error
+	DcpNoOp               func(req *DcpNoOpEvent) (*DcpNoOpEventResponse, error)
 }
 
 type UnsolicitedOpsParser struct {
+	CollectionsEnabled bool
+}
+
+func (o UnsolicitedOpsParser) decodeExtFrames(
+	buf []byte,
+) (uint16, error) {
+	var streamId uint16
+	err := IterExtFrames(buf, func(code ExtFrameCode, data []byte) {
+		if code == ExtFrameCodeReqStreamID {
+			streamId = binary.BigEndian.Uint16(data[0:])
+		}
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return streamId, nil
+}
+
+func (o UnsolicitedOpsParser) decodeCollectionAndKey(key []byte) (uint32, []byte, error) {
+	if !o.CollectionsEnabled {
+		return 0, key, nil
+	}
+
+	return DecodeCollectionIDAndKey(key)
+}
+
+func (o UnsolicitedOpsParser) parseDcpSnapshotMarker(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpSnapshotMarker == nil {
+		return errors.New("unhandled DcpSnapshotMarker event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 20 {
+		// Length of 20 indicates a v1 packet
+		return handlers.DcpSnapshotMarker(&DcpSnapshotMarkerEvent{
+			StreamId:           streamId,
+			Version:            1,
+			StartSeqNo:         binary.BigEndian.Uint64(pak.Extras[0:]),
+			EndSeqNo:           binary.BigEndian.Uint64(pak.Extras[8:]),
+			SnapshotType:       DcpSnapshotState(binary.BigEndian.Uint32(pak.Extras[16:])),
+			MaxVisibleSeqNo:    0,
+			HighCompletedSeqNo: 0,
+			SnapshotTimeStamp:  0,
+		})
+	} else if len(pak.Extras) == 1 {
+		// Length of 1 indicates v2+ packet
+		version := pak.Extras[0]
+		if version == 0 {
+			return handlers.DcpSnapshotMarker(&DcpSnapshotMarkerEvent{
+				StreamId:           streamId,
+				Version:            2,
+				StartSeqNo:         binary.BigEndian.Uint64(pak.Extras[0:]),
+				EndSeqNo:           binary.BigEndian.Uint64(pak.Extras[8:]),
+				SnapshotType:       DcpSnapshotState(binary.BigEndian.Uint32(pak.Extras[16:])),
+				MaxVisibleSeqNo:    binary.BigEndian.Uint64(pak.Value[20:]),
+				HighCompletedSeqNo: binary.BigEndian.Uint64(pak.Value[28:]),
+				SnapshotTimeStamp:  0,
+			})
+		} else if version == 1 {
+			return handlers.DcpSnapshotMarker(&DcpSnapshotMarkerEvent{
+				StreamId:           streamId,
+				Version:            3,
+				StartSeqNo:         binary.BigEndian.Uint64(pak.Extras[0:]),
+				EndSeqNo:           binary.BigEndian.Uint64(pak.Extras[8:]),
+				SnapshotType:       DcpSnapshotState(binary.BigEndian.Uint32(pak.Extras[16:])),
+				MaxVisibleSeqNo:    binary.BigEndian.Uint64(pak.Value[20:]),
+				HighCompletedSeqNo: binary.BigEndian.Uint64(pak.Value[28:]),
+				SnapshotTimeStamp:  binary.BigEndian.Uint64(pak.Value[36:]),
+			})
+		} else {
+			return &protocolError{"unknown dcp snapshot marker version"}
+		}
+	} else {
+		return &protocolError{"unknown dcp snapshot marker format"}
+	}
+}
+
+func (o UnsolicitedOpsParser) parseDcpMutation(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpMutation == nil {
+		return errors.New("unhandled DcpMutation event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	collectionId, key, err := o.decodeCollectionAndKey(pak.Key)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 31 {
+		return handlers.DcpMutation(&DcpMutationEvent{
+			StreamId:     streamId,
+			Version:      1,
+			SeqNo:        binary.BigEndian.Uint64(pak.Extras[0:]),
+			RevNo:        binary.BigEndian.Uint64(pak.Extras[8:]),
+			Flags:        binary.BigEndian.Uint32(pak.Extras[16:]),
+			Expiry:       binary.BigEndian.Uint32(pak.Extras[20:]),
+			LockTime:     binary.BigEndian.Uint32(pak.Extras[24:]),
+			MetaDataSize: binary.BigEndian.Uint16(pak.Extras[28:]),
+			NRU:          pak.Extras[30],
+			Cas:          pak.Cas,
+			Datatype:     pak.Datatype,
+			VbucketId:    pak.VbucketID,
+			CollectionId: collectionId,
+			Key:          key,
+			Value:        pak.Value,
+		})
+	}
+
+	return &protocolError{"unknown dcp mutation format"}
+}
+
+func (o UnsolicitedOpsParser) parseDcpDeletion(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpDeletion == nil {
+		return errors.New("unhandled DcpDeletion event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	collectionId, key, err := o.decodeCollectionAndKey(pak.Key)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 18 {
+		return handlers.DcpDeletion(&DcpDeletionEvent{
+			StreamId:     streamId,
+			Version:      1,
+			SeqNo:        binary.BigEndian.Uint64(pak.Extras[0:]),
+			RevNo:        binary.BigEndian.Uint64(pak.Extras[8:]),
+			Cas:          pak.Cas,
+			Datatype:     pak.Datatype,
+			VbucketId:    pak.VbucketID,
+			CollectionId: collectionId,
+			Key:          key,
+			DeleteTime:   0,
+		})
+	} else if len(pak.Extras) == 21 {
+		return handlers.DcpDeletion(&DcpDeletionEvent{
+			StreamId:     streamId,
+			Version:      2,
+			SeqNo:        binary.BigEndian.Uint64(pak.Extras[0:]),
+			RevNo:        binary.BigEndian.Uint64(pak.Extras[8:]),
+			Cas:          pak.Cas,
+			Datatype:     pak.Datatype,
+			VbucketId:    pak.VbucketID,
+			CollectionId: collectionId,
+			Key:          key,
+			DeleteTime:   binary.BigEndian.Uint32(pak.Extras[16:]),
+		})
+	}
+
+	return &protocolError{"unknown dcp mutation format"}
+}
+
+func (o UnsolicitedOpsParser) parseDcpExpiration(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpExpiration == nil {
+		return errors.New("unhandled DcpExpiration event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	collectionId, key, err := o.decodeCollectionAndKey(pak.Key)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 20 {
+		return handlers.DcpExpiration(&DcpExpirationEvent{
+			StreamId:     streamId,
+			Version:      2,
+			SeqNo:        binary.BigEndian.Uint64(pak.Extras[0:]),
+			RevNo:        binary.BigEndian.Uint64(pak.Extras[8:]),
+			Cas:          pak.Cas,
+			Datatype:     pak.Datatype,
+			VbucketId:    pak.VbucketID,
+			CollectionId: collectionId,
+			Key:          key,
+			DeleteTime:   binary.BigEndian.Uint32(pak.Extras[16:]),
+		})
+	}
+
+	return &protocolError{"unknown dcp mutation format"}
+}
+
+func (o UnsolicitedOpsParser) parseDcpEvent(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 13 {
+		seqNo := binary.BigEndian.Uint64(pak.Extras[0:])
+		eventCode := DcpStreamEventCode(binary.BigEndian.Uint32(pak.Extras[8:]))
+		version := pak.Extras[12]
+
+		if eventCode == DcpStreamEventCodeCollectionCreate {
+			if handlers.DcpCollectionCreation == nil {
+				return errors.New("unhandled DcpCollectionCreation event")
+			}
+
+			if version == 0 {
+				return handlers.DcpCollectionCreation(&DcpCollectionCreationEvent{
+					StreamId:       streamId,
+					Version:        0,
+					SeqNo:          seqNo,
+					VbucketId:      pak.VbucketID,
+					ManifestUid:    binary.BigEndian.Uint64(pak.Value[0:]),
+					ScopeId:        binary.BigEndian.Uint32(pak.Value[8:]),
+					CollectionId:   binary.BigEndian.Uint32(pak.Value[12:]),
+					CollectionName: string(pak.Key),
+					Ttl:            0,
+				})
+			} else if version == 1 {
+				return handlers.DcpCollectionCreation(&DcpCollectionCreationEvent{
+					StreamId:       streamId,
+					Version:        1,
+					SeqNo:          seqNo,
+					VbucketId:      pak.VbucketID,
+					ManifestUid:    binary.BigEndian.Uint64(pak.Value[0:]),
+					ScopeId:        binary.BigEndian.Uint32(pak.Value[8:]),
+					CollectionId:   binary.BigEndian.Uint32(pak.Value[12:]),
+					CollectionName: string(pak.Key),
+					Ttl:            binary.BigEndian.Uint32(pak.Value[16:]),
+				})
+			} else {
+				return &protocolError{"unknown dcp collection create event version"}
+			}
+		} else if eventCode == DcpStreamEventCodeCollectionDelete {
+			if handlers.DcpCollectionDeletion == nil {
+				return errors.New("unhandled DcpCollectionDeletion event")
+			}
+
+			if version == 0 {
+				return handlers.DcpCollectionDeletion(&DcpCollectionDeletionEvent{
+					StreamId:     streamId,
+					Version:      0,
+					SeqNo:        seqNo,
+					VbucketId:    pak.VbucketID,
+					ManifestUid:  binary.BigEndian.Uint64(pak.Value[0:]),
+					ScopeID:      binary.BigEndian.Uint32(pak.Value[8:]),
+					CollectionId: binary.BigEndian.Uint32(pak.Value[12:]),
+				})
+			} else {
+				return &protocolError{"unknown dcp collection delete event version"}
+			}
+		} else if eventCode == DcpStreamEventCodeCollectionFlush {
+			if handlers.DcpCollectionFlush == nil {
+				return errors.New("unhandled DcpCollectionFlush event")
+			}
+
+			if version == 0 {
+				return handlers.DcpCollectionFlush(&DcpCollectionFlushEvent{
+					StreamId:     streamId,
+					Version:      0,
+					SeqNo:        seqNo,
+					VbucketId:    pak.VbucketID,
+					ManifestUid:  binary.BigEndian.Uint64(pak.Value[0:]),
+					CollectionId: binary.BigEndian.Uint32(pak.Value[8:]),
+				})
+			} else {
+				return &protocolError{"unknown dcp collection flush event version"}
+			}
+		} else if eventCode == DcpStreamEventCodeScopeCreate {
+			if handlers.DcpScopeCreation == nil {
+				return errors.New("unhandled DcpScopeCreation event")
+			}
+
+			if version == 0 {
+				return handlers.DcpScopeCreation(&DcpScopeCreationEvent{
+					StreamId:    streamId,
+					Version:     0,
+					SeqNo:       seqNo,
+					VbucketId:   pak.VbucketID,
+					ManifestUid: binary.BigEndian.Uint64(pak.Value[0:]),
+					ScopeId:     binary.BigEndian.Uint32(pak.Value[8:]),
+					ScopeName:   string(pak.Key),
+				})
+			} else {
+				return &protocolError{"unknown dcp scope create event version"}
+			}
+		} else if eventCode == DcpStreamEventCodeScopeDelete {
+			if handlers.DcpScopeDeletion == nil {
+				return errors.New("unhandled DcpScopeDeletion event")
+			}
+
+			if version == 0 {
+				return handlers.DcpScopeDeletion(&DcpScopeDeletionEvent{
+					StreamId:    streamId,
+					Version:     0,
+					SeqNo:       seqNo,
+					VbucketId:   pak.VbucketID,
+					ManifestUid: binary.BigEndian.Uint64(pak.Value[0:]),
+					ScopeId:     binary.BigEndian.Uint32(pak.Value[8:]),
+				})
+			} else {
+				return &protocolError{"unknown dcp scope delete event version"}
+			}
+		} else if eventCode == DcpStreamEventCodeCollectionChanged {
+			if handlers.DcpCollectionChanged == nil {
+				return errors.New("unhandled DcpCollectionChanged event")
+			}
+
+			if version == 0 {
+				return handlers.DcpCollectionChanged(&DcpCollectionModificationEvent{
+					StreamId:     streamId,
+					Version:      0,
+					SeqNo:        seqNo,
+					VbucketId:    pak.VbucketID,
+					ManifestUid:  binary.BigEndian.Uint64(pak.Value[0:]),
+					CollectionId: binary.BigEndian.Uint32(pak.Value[8:]),
+					Ttl:          binary.BigEndian.Uint32(pak.Value[8:]),
+				})
+			} else {
+				return &protocolError{"unknown dcp collection changed event version"}
+			}
+		} else {
+			return &protocolError{"unknown dcp event code"}
+		}
+	}
+
+	return &protocolError{"unknown dcp event format"}
+}
+
+func (o UnsolicitedOpsParser) parseDcpStreamEnd(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpStreamEnd == nil {
+		return errors.New("unhandled DcpStreamEnd event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	return handlers.DcpStreamEnd(&DcpStreamEndEvent{
+		StreamId:  streamId,
+		VbucketId: pak.VbucketID,
+	})
+}
+
+func (o UnsolicitedOpsParser) parseDcpOsoSnapshot(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpOSOSnapshot == nil {
+		return errors.New("unhandled DcpOSOSnapshot event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 4 {
+		return handlers.DcpOSOSnapshot(&DcpOSOSnapshotEvent{
+			StreamId:     streamId,
+			VbucketId:    pak.VbucketID,
+			SnapshotType: binary.BigEndian.Uint32(pak.Extras[0:]),
+		})
+	}
+
+	return &protocolError{"unknown dcp oso snapshot format"}
+}
+
+func (o UnsolicitedOpsParser) parseDcpSeqNoAdvance(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpSeqNoAdvanced == nil {
+		return errors.New("unhandled DcpSeqNoAdvanced event")
+	}
+
+	streamId, err := o.decodeExtFrames(pak.FramingExtras)
+	if err != nil {
+		return err
+	}
+
+	if len(pak.Extras) == 4 {
+		return handlers.DcpSeqNoAdvanced(&DcpSeqNoAdvancedEvent{
+			StreamId:  streamId,
+			VbucketId: pak.VbucketID,
+			SeqNo:     binary.BigEndian.Uint64(pak.Extras[0:]),
+		})
+	}
+
+	return &protocolError{"unknown dcp oso snapshot format"}
 }
 
 func (o UnsolicitedOpsParser) parseSrvClustermapChangeNotification(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
@@ -37,11 +450,57 @@ func (o UnsolicitedOpsParser) parseSrvClustermapChangeNotification(pak *Packet, 
 	return &protocolError{"unknown clustermap change notification format"}
 }
 
-func (o UnsolicitedOpsParser) Handle(pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+func (o UnsolicitedOpsParser) writeDcpNoOpResponse(r Replier, pak *Packet, _ *DcpNoOpEventResponse) error {
+	return r.WritePacket(&Packet{
+		Magic:  MagicRes,
+		OpCode: OpCodeDcpNoOp,
+		Opaque: pak.Opaque,
+		Status: StatusSuccess,
+	})
+}
+
+func (o UnsolicitedOpsParser) parseDcpNoOp(r Replier, pak *Packet, handlers *UnsolicitedOpsHandlers) error {
+	if handlers.DcpNoOp == nil {
+		return errors.New("unhandled DcpNoOp event")
+	}
+
+	resp, err := handlers.DcpNoOp(&DcpNoOpEvent{})
+	if err != nil {
+		return err
+	}
+
+	if resp == nil {
+		return nil
+	}
+
+	return o.writeDcpNoOpResponse(r, pak, resp)
+}
+
+func (o UnsolicitedOpsParser) Handle(r Replier, pak *Packet, handlers *UnsolicitedOpsHandlers) error {
 	if pak.Magic.IsRequest() {
 		if pak.Magic.IsServerInitiated() {
 			if pak.OpCode == OpCodeSrvClustermapChangeNotification {
 				return o.parseSrvClustermapChangeNotification(pak, handlers)
+			}
+		} else {
+			if pak.OpCode == OpCodeDcpSnapshotMarker {
+				return o.parseDcpSnapshotMarker(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpMutation {
+				return o.parseDcpMutation(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpDeletion {
+				return o.parseDcpDeletion(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpExpiration {
+				return o.parseDcpExpiration(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpEvent {
+				return o.parseDcpEvent(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpStreamEnd {
+				return o.parseDcpStreamEnd(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpOsoSnapshot {
+				return o.parseDcpOsoSnapshot(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpSeqNoAdvanced {
+				return o.parseDcpSeqNoAdvance(pak, handlers)
+			} else if pak.OpCode == OpCodeDcpNoOp {
+				return o.parseDcpNoOp(r, pak, handlers)
 			}
 		}
 	}
