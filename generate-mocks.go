79092756ebd638cde1fff01fdf5da295c6c45f6a
Brett Lawson <brett19@gmail.com>
2025-11-06 15:20:54 -0800

    ING-1337: Generate mocks with the correct file formatting.

--- generate-mocks.go
+++ generate-mocks.go
@@ -1,11 +1,11 @@
-//go:generate moq -out mock_collectionresolver_test.go . CollectionResolver
-//go:generate moq -out mock_vbucketrouter_test.go . VbucketRouter
-//go:generate moq -out mock_kvclient_test.go . KvClient MemdxClient
-//go:generate moq -out mock_kvclientpool_test.go . KvClientPool
-//go:generate moq -out mock_kvclientbabysitter_test.go . KvClientBabysitter
-//go:generate moq -out mock_kvendpointclientmanager_test.go . KvEndpointClientManager
-//go:generate moq -out mock_retrymanager_test.go . RetryManager RetryController
-//go:generate moq -out mock_bucketchecker_test.go . BucketChecker
-//go:generate moq -out ./cbauthx/mock_authcheck_test.go ./cbauthx AuthCheck
+//go:generate moq -fmt goimports -out mock_collectionresolver_test.go . CollectionResolver
+//go:generate moq -fmt goimports -out mock_vbucketrouter_test.go . VbucketRouter
+//go:generate moq -fmt goimports -out mock_kvclient_test.go . KvClient MemdxClient
+//go:generate moq -fmt goimports -out mock_kvclientpool_test.go . KvClientPool
+//go:generate moq -fmt goimports -out mock_kvclientbabysitter_test.go . KvClientBabysitter
+//go:generate moq -fmt goimports -out mock_kvendpointclientmanager_test.go . KvEndpointClientManager
+//go:generate moq -fmt goimports -out mock_retrymanager_test.go . RetryManager RetryController
+//go:generate moq -fmt goimports -out mock_bucketchecker_test.go . BucketChecker
+//go:generate moq -fmt goimports -out ./cbauthx/mock_authcheck_test.go ./cbauthx AuthCheck
 
 package gocbcorex
